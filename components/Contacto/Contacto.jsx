47a76c36678d070d3b011aeb191ab94409b7616c
AngeloOz <angelo-mjz7@hotmail.com>
2022-04-22 17:23:45 -0500

    feat: footer landing

--- components/Contacto/Contacto.jsx
+++ components/Contacto/Contacto.jsx
@@ -3,10 +3,10 @@ import css from "../../styles/Contacto.module.scss";
 export const Contacto = () => {
   return (
     <section className={css.contenedor_contacto} id="contacto">
-      <div className="container-fluid">
-        <div className="row g-0">
-          <div className="col-6">
-            <div className={css.contenedor_mapa}>
+      <div className="container-fluid" data-height-100>
+        <div className="row g-0 d-flex  flex-column-reverse" data-height-100>
+          <div className="col-12 col-md-6" data-height-100>
+            <div className={css.contenedor_mapa} data-height-100>
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d255267.41257061885!2d-78.3342501540625!3d-1.442838173714416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sfogon%20de%20coz!5e0!3m2!1ses-419!2sec!4v1650577331160!5m2!1ses-419!2sec"
                 loading="lazy"
@@ -14,40 +14,41 @@ export const Contacto = () => {
               ></iframe>
             </div>
           </div>
-          <div className="col-6">
+          <div className="col-12 col-md-6">
             <div className={css.contenedor_form}>
               <div className={css.text_contect}>
                 <h2>PÓNGANSE EN CONTACTO CON NOSOTROS</h2>
+                <p>Su dirección de correo electrónico no será publicada. Los campos obligatorios están marcados *</p>
               </div>
               <form>
-                <div className="row mt-2">
+                <div className="row mt-3">
                   <div className="col">
                     <input
                       type="text"
                       className="form-control"
-                      placeholder="Nombres y Apellidos"
+                      placeholder="Nombres y Apellidos*"
                     />
                   </div>
                 </div>
-                <div className="row mt-2">
+                <div className="row mt-3">
                   <div className="col">
                     <input
                       type="email"
                       className="form-control"
-                      placeholder="Correo electrónico"
+                      placeholder="Correo electrónico*"
                     />
                   </div>
                 </div>
-                <div className="row mt-2">
+                <div className="row mt-3">
                   <div className="col">
                     <input
                       type="telf"
                       className="form-control"
-                      placeholder="Télefono"
+                      placeholder="Télefono*"
                     />
                   </div>
                 </div>
-                <div className="row mt-2">
+                <div className="row mt-3">
                   <div className="col">
                     <textarea
                       className="form-control"
@@ -55,7 +56,7 @@ export const Contacto = () => {
                     ></textarea>
                   </div>
                 </div>
-                <div className="row mt-2">
+                <div className="row mt-3">
                   <div className="col">
                     <button
                       type="submit"
