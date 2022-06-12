205d851cac357a59066c295101aa3eb2f0abe57d
AngeloOz <angelo-mjz7@hotmail.com>
2022-06-12 14:24:50 -0500

    as

--- pages/orders/history.jsx
+++ pages/orders/history.jsx
@@ -15,22 +15,6 @@ const HistoryPage = () => {
     //   <Typography variant="h1" component="h1">
     //     Historial de ordenes
     //   </Typography>
-
-<<<<<<< HEAD
-    //   <Grid container>
-    //     <Grid item xs={12} sx={{ height: 650, width: "100%" }}>
-
-    //     </Grid>
-    //   </Grid>
-    // </ShopLayout>
-=======
-      <Grid container>
-        <Grid item xs={12} sx={{ height: 650, width: "100%" }}>
-
-        </Grid>
-      </Grid>
-    </ShopLayout>
->>>>>>> 883c54c36cfb0a7a171063b95ce86a5d4d71b0be
   );
 };
 
