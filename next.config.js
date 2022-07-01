80591fd1fd67b4b8ed4bc0a9da33572e378c7886
AngeloOz <angelo-mjz7@hotmail.com>
2022-07-01 09:33:35 -0500

    implementacion de PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['localhost', 'res.cloudinary.com','assets.website-files.com',"*"]
-  }
-}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache')
+
+
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching
+  },
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['localhost', 'res.cloudinary.com', 'assets.website-files.com', "*"]
+  }
+})
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['localhost', 'res.cloudinary.com','assets.website-files.com',"*"]
+//   },
+//   eslint:{
+//     ignoreDuringBuilds: true
+//   }
+// }
+
+// module.exports = nextConfig
